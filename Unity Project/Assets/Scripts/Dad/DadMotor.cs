6d4b5adda5d34f6b197b3a7a101bbf39053a8231
NathanSaidas <NathanSaidas@users.noreply.github.com>
2014-12-13 15:13:40 -0500

    Changes:
    • Adding temp character controller to project.
    • Adding in basic dad follow AI

---
+++ Unity Project/Assets/Scripts/Dad/DadMotor.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DadMotor : MonoBehaviour
+{
+    private enum State
+    {
+        Idle,
+        Waiting,
+        Attacking,
+        MovingToGoal
+    }
+
+    [SerializeField]
+    private Transform m_Player = null;
+
+    private Vector3 m_Target = Vector3.zero;
+    [SerializeField]
+    private float m_Timer = 1.0f;
+    [SerializeField]
+    private float m_MovementSpeed = 5.0f;
+    private float m_CurrentTime = 0.0f;
+
+    [SerializeField]
+    private Transform m_WaypointRoot = null;
+    [SerializeField]
+    private List<Transform> m_Waypoints = new List<Transform>();
+    [SerializeField]
+    private Transform m_TargetWayPoint = null;
+
+
+	// Use this for initialization
+	void Start () 
+    {
+        if(m_WaypointRoot != null)
+        {
+            foreach(Transform child in m_WaypointRoot)
+            {
+                m_Waypoints.Add(child);
+            }
+        }
+
+        UpdateTarget();
+	}
+	
+	void Update()
+    {
+        m_CurrentTime += Time.deltaTime;
+        if(m_CurrentTime > m_Timer)
+        {
+            UpdateTarget();
+        }
+        Vector3 direction = (m_Target - transform.position).normalized;
+        Vector3 position = transform.position + direction * m_MovementSpeed * Time.deltaTime;
+        float distanceA = Vector3.Distance(m_Target, transform.position);
+        float distanceB = Vector3.Distance(m_Target, position);
+        if(distanceB > distanceA)
+        {
+            transform.position = m_Target;
+        }
+        else
+        {
+            transform.position = position;
+        }
+        
+    }
+
+    void UpdateTarget()
+    {
+        m_Target = m_Player.transform.position;
+        m_CurrentTime = 0.0f;
+        GetTargetWaypoint();
+        if (m_TargetWayPoint != null)
+        {
+            m_Target = m_TargetWayPoint.position;
+        }
+    }
+
+    void GetTargetWaypoint()
+    {
+        Transform bestWaypoint = null;
+        float shortestDistance = float.MaxValue;
+        foreach(Transform waypoint in m_Waypoints)
+        {
+            float distance = (m_Target - waypoint.position).sqrMagnitude;
+            if(distance < shortestDistance)
+            {
+                bestWaypoint = waypoint;
+                shortestDistance = distance;
+            }
+        }
+        m_TargetWayPoint = bestWaypoint;
+    }
+}
---
+++ Unity Project/Assets/Scripts/TempController.cs
@@ -0,0 +1,107 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class TempController : MonoBehaviour 
+{
+    [SerializeField]
+    private float m_MovementSpeed = 3.0f;
+    [SerializeField]
+    private float m_JumpForce = 5.0f;
+    [SerializeField]
+    private float m_MaxMovementSpeed = 3.0f;
+    [SerializeField]
+    private float m_CollisionError = 0.1f;
+
+
+    [SerializeField]
+    private bool m_IsGrounded = false;
+    private Vector2 m_NetForce = Vector2.zero;
+
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        float hInput = Input.GetAxis("Horizontal");
+        float vInput = Input.GetAxis("Vertical");
+        bool jump = Input.GetButton("Jump");
+
+        //float yForce = rigidbody2D.velocity.y;
+        //rigidbody2D.velocity = new Vector2(hInput * m_MovementSpeed * Time.deltaTime, yForce);
+        rigidbody2D.AddForce(new Vector2(hInput * m_MovementSpeed,0.0f), ForceMode2D.Impulse);
+        if(jump && m_IsGrounded)
+        {
+            rigidbody2D.AddForce(new Vector2(0.0f, m_JumpForce), ForceMode2D.Impulse);
+        }
+
+
+        rigidbody2D.AddForce(m_NetForce);
+        rigidbody2D.velocity = new Vector2(Mathf.Clamp(rigidbody2D.velocity.x, -m_MaxMovementSpeed, m_MaxMovementSpeed), rigidbody2D.velocity.y);
+        rigidbody2D.angularVelocity = 0.0f;
+        transform.rotation = Quaternion.identity;
+	}
+
+    void FixedUpdate()
+    {
+        Vector2 topLeft = transform.position;
+        Vector2 bottomRight = transform.position;
+
+        BoxCollider2D bCol = collider2D as BoxCollider2D;
+        if(bCol != null)
+        {
+            topLeft.x += -bCol.size.x * 0.5f - m_CollisionError;
+            topLeft.y += bCol.size.y * 0.5f + m_CollisionError;
+            bottomRight.x += bCol.size.x * 0.5f + m_CollisionError;
+            bottomRight.y += -bCol.size.y * 0.5f - m_CollisionError;
+        }
+
+        int layerMask = 1 <<  gameObject.layer;
+        layerMask = ~layerMask;
+
+        Collider2D col = Physics2D.OverlapArea(topLeft, bottomRight, layerMask );
+
+        if (col != null )
+        {
+            m_IsGrounded = true;
+            if(col.transform.position.y > transform.position.y)
+            {
+                m_IsGrounded = false;
+            }
+        }
+        else
+        {
+            m_IsGrounded = false;
+        }
+        rigidbody2D.angularVelocity = 0.0f;
+    }
+
+    void AddForce(Vector2 aForce)
+    {
+        m_NetForce += aForce;
+    }
+    
+    void OnDrawGizmos()
+    {
+        Vector2 topLeft = transform.position;
+        Vector2 bottomRight = transform.position;
+
+        BoxCollider2D bCol = collider2D as BoxCollider2D;
+        if (bCol != null)
+        {
+            topLeft.x += -bCol.size.x * 0.5f - m_CollisionError;
+            topLeft.y += bCol.size.y * 0.5f + m_CollisionError;
+            bottomRight.x += bCol.size.x * 0.5f + m_CollisionError;
+            bottomRight.y += -bCol.size.y * 0.5f - m_CollisionError;
+        }
+
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawLine(topLeft, bottomRight);
+
+    }
+}
