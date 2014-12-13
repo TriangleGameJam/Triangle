ce17f1d812afbfefd9b2a2291b6b506196530e94
NathanSaidas <NathanSaidas@users.noreply.github.com>
2014-12-12 20:06:00 -0500

    Changes:
    • Adding example MonoBehaviour

---
+++ Unity Project/Assets/Scripts/Example.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Example : MonoBehaviour
+{
+
+    void Awake()
+    {
+
+    }
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+    void LateUpdate()
+    {
+
+    }
+    void FixedUpdate()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider aCollider)
+    {
+
+    }
+    void OnTriggerExit(Collider aCollider)
+    {
+
+    }
+    void OnTriggerStay(Collider aCollider)
+    {
+
+    }
+}
