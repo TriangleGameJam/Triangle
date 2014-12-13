c2014cbbcbe433357bcc84a8da96628965ba2809
NathanSaidas <NathanSaidas@users.noreply.github.com>
2014-12-12 20:24:37 -0500

    Changes:
    • Added some example code.

--- Unity Project/Assets/Scripts/Example.cs
+++ Unity Project/Assets/Scripts/Example.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class Example : MonoBehaviour
 {
+    public float m_MoveSpeed = 5.0f;
+    public float m_JumpForce = 250.0f;
 
     void Awake()
     {
@@ -11,13 +13,32 @@ public class Example : MonoBehaviour
 	// Use this for initialization
 	void Start () 
     {
-	
+
 	}
 	
 	// Update is called once per frame
 	void Update () 
     {
-	
+        bool moveRight = Input.GetKey(KeyCode.D) | Input.GetKey(KeyCode.RightArrow);
+        bool moveLeft = Input.GetKey(KeyCode.A) | Input.GetKey(KeyCode.LeftArrow);
+        bool jump = Input.GetKeyDown(KeyCode.Space);
+
+        if(moveRight)
+        {
+            Debug.Log("Moving Right");
+            rigidbody2D.AddForce(Vector2.right * m_MoveSpeed * Time.deltaTime,ForceMode2D.Impulse);
+        }
+        if(moveLeft)
+        {
+            Debug.Log("Moving Left");
+            rigidbody2D.AddForce(-Vector2.right * m_MoveSpeed * Time.deltaTime, ForceMode2D.Impulse);
+        }
+        if(jump)
+        {
+            Debug.Log("Jumping");
+            rigidbody2D.AddForce(Vector2.up * m_JumpForce * Time.deltaTime, ForceMode2D.Impulse);
+        }
+
 	}
     void LateUpdate()
     {
