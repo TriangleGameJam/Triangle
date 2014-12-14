974cee5452e4a924b19785a35dfea54d2e8c936a
Riza Baltazar <rizbalt@gmail.com>
2014-12-14 12:49:52 -0500

    Revert "Revert "Animations :baby:""
    This reverts commit 625ca4d362c7e875c3f1954ab00b40829f6ca52a.

---
+++ Unity Project/Assets/Scripts/Character/AnimationExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public static class AnimationExtensions
+{
+    public static IEnumerator WhilePlaying(this Animation animation)
+    {
+        do
+        {
+            yield return null;
+        } while (animation.isPlaying);
+    }
+
+    public static IEnumerator WhilePlaying(this Animation animation, string animationName)
+    {
+        animation.PlayQueued(animationName);
+        yield return animation.WhilePlaying();
+    }
+}
--- Unity Project/Assets/Scripts/Character/PlayerCombos.cs
+++ Unity Project/Assets/Scripts/Character/PlayerCombos.cs
@@ -4,7 +4,7 @@ public static class PlayerCombos
     public const string SHOULDER_SHRUG = "Q";
     public const string SASS_BLAST = "W";
     public const string JAYZ = "WEERQQ";
-    public const string TABLE_FLIP = "WWERQR";
+    public const string TABLE_FLIP = "WWEQRQ";
     public const string WHATEVA_WAVE = "E";
     public const string IM_NOT_LISTENING = "EQRQ";
     public const string WALK_AWAY = "EEQW";
